5730544dc62b50d113cbada7fb63c9a0ff0b55ef
Adrien Ball <adrien.ball@snips.net>
2017-01-14 18:30:30 +0100

    Implement first intelligent strategy

--- MyBot.py
+++ MyBot.py
@@ -1,12 +1,53 @@
 import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
-
+import math
+import logging
 
 myID, game_map = hlt.get_init()
+# logging.basicConfig(filename='debug.log', filemode='w', level=logging.DEBUG)
 hlt.send_init("MyPythonBot")
+
+# strengths = [square.strength for square in game_map]
+# logging.debug(strengths)
+
+
+def assign_move(square):
+    if square.strength == 0:
+        return Move(square, STILL)
+
+    sorted_moves = [NORTH, EAST, SOUTH, WEST]
+    sorted_moves.sort(key=lambda move: -direction_opportunity(square, move))
+
+    target_square = game_map.get_target(square, sorted_moves[0])
+
+    if is_move_possible(square, target_square):
+        return Move(square, sorted_moves[0])
+    else:
+        return Move(square, STILL)
+
+
+def is_move_possible(source_square, target_square):
+    return target_square.owner == myID or source_square.strength >= target_square.strength
+
+
+def direction_opportunity(square, direction):
+    opportunity = 0
+    decay = 1
+    decay_factor = math.exp(-1 / 5.0)
+    current_square = square
+    for i in range(game_map.width):
+        neighbor = game_map.get_target(current_square, direction)
+        if neighbor.owner != myID:
+            opportunity -= neighbor.strength * decay
+        else:
+            opportunity -= 255 * decay
+        current_square = neighbor
+        decay *= decay_factor
+    return opportunity
+
 
 while True:
     game_map.get_frame()
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
+    moves = [assign_move(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
