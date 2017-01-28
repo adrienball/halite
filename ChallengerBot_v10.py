a3b66e716ae7c1eeb9845f7c3a3082639cc28755
Adrien Ball <adrien.ball@snips.net>
2017-01-28 21:23:43 +0100

    Clean v10 bot

--- ChallengerBot_v10.py
+++ ChallengerBot_v10.py
@@ -1,22 +1,19 @@
 import hlt
-from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
+from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move
 from itertools import groupby
 import math
 import os
 
+MAX_STRENGTH = 255
 
-def assign_move(square, available_directions={NORTH, EAST, SOUTH, WEST}):
+
+def get_best_individual_move(square, available_directions={NORTH, EAST, SOUTH, WEST}):
     if len(available_directions) == 0 or square.strength <= 5 * square.production:
         return Move(square, STILL)
 
     if len(available_directions) > 1:
-        directions_with_opportunity = [
-            {"direction": direction, "opportunity": direction_opportunity(square, direction)}
-            for direction in available_directions
-            ]
-        directions_with_opportunity.sort(key=lambda item: -item["opportunity"])
-
-        best_opportunity_direction = directions_with_opportunity[0]["direction"]
+        best_opportunity_direction = max(available_directions,
+                                         key=lambda direction: direction_opportunity(square, direction))
     else:
         best_opportunity_direction = list(available_directions)[0]
 
@@ -28,29 +25,8 @@ def assign_move(square, available_directions={NORTH, EAST, SOUTH, WEST}):
         return Move(square, STILL)
 
 
-def find_nearest_enemy_direction(square):
-    direction = NORTH
-    max_distance = min(game_map.width, game_map.height) / 2
-    for d in (NORTH, EAST, SOUTH, WEST):
-        distance = 0
-        current = square
-        while current.owner == myID and distance < max_distance:
-            distance += 1
-            current = game_map.get_target(current, d)
-        if distance < max_distance:
-            direction = d
-            max_distance = distance
-    return direction
-
-
-def distance_to_enemy(square, direction):
-    distance = 0
-    max_distance = min(game_map.width, game_map.height) / 2
-    current = square
-    while current.owner == myID and distance < max_distance:
-        distance += 1
-        current = game_map.get_target(current, direction)
-    return distance
+def is_direction_possible(source_square, direction):
+    return is_move_possible(source_square, game_map.get_target(source_square, direction))
 
 
 def is_move_possible(source_square, target_square):
@@ -63,31 +39,24 @@ def is_move_possible(source_square, target_square):
 
 
 def direction_opportunity(square, direction):
-    if direction == STILL:
-        return still_opportunity(square)
-    else:
-        return move_opportunity(square, direction)
+    return still_opportunity(square) if direction == STILL else move_opportunity(square, direction)
 
 
-# TODO: improve opportunity definition by taking into account distance to frontier
 def still_opportunity(square):
-    # distances = [distance_to_enemy(square, direction) for direction in (NORTH, EAST, SOUTH, WEST)]
-    # frontier_distance = max(distances)
-    # max_distance = min(game_map.width, game_map.height) / 2
-    return square_opportunity(square.strength, square.production)  # * (1 - frontier_distance / max_distance)
+    return square_opportunity(square.strength, square.production)
 
 
 def square_opportunity(strength, production):
-    normalized_strength = (strength + 1) / 256
-    normalized_production = (production + 1) / (max_production + 1)
+    normalized_strength = (strength + 1) / (MAX_STRENGTH + 1)
+    normalized_production = (production + 1) / (MAX_PRODUCTION + 1)
     opportunity = normalized_production / normalized_strength
     return opportunity
 
 
 def move_opportunity(square, direction):
-    # TODO: this can be simplified by computing the closed form of the sums
-    # distance = distance_to_enemy(square, direction)
-    max_distance = min(game_map.width, game_map.height) / 2
+    """
+    :return: a float that represents the opportunity of moving to `direction` from `square`
+    """
     opportunity = 0
     current_weight = 1
     decay_factor = math.exp(-1 / 2.0)
@@ -101,10 +70,13 @@ def move_opportunity(square, direction):
         current_square = neighbor
         total_weight += current_weight
         current_weight *= decay_factor
-    return (opportunity / total_weight)  # * (1 - distance / max_distance)
+    return opportunity / total_weight
 
 
 def get_unowned_id():
+    """
+    :return: the id of unowned squares
+    """
     owners = [square.owner for square in game_map]
     max_owner_id = myID
     max_owner_count = 0
@@ -116,33 +88,31 @@ def get_unowned_id():
     return max_owner_id
 
 
-myID, game_map = hlt.get_init()
-productions = [square.production for square in game_map]
-max_production = max(productions)
-unowned_id = get_unowned_id()
-bot_name = os.path.basename(__file__).split('.')[0]
-hlt.send_init(bot_name)
+def get_best_collective_moves():
+    """
+    Compute the best collective moves by processing the stack of best individual moves and re-affecting
+    self-destructive moves, i.e. moves that have a cumulative strength > MAX_STRENGTH.
+    :return: a list of moves that are collectively optimized
+    """
 
-while True:
-    game_map.get_frame()
+    # TODO: Replace dictionary with matrix here
     targets = {}
-
     squares_stack = [{'square': square, 'available_directions': {NORTH, EAST, SOUTH, WEST}} for square in game_map if
                      square.owner == myID]
 
     while len(squares_stack) > 0:
-        square_with_available_directions = squares_stack.pop()
-        square = square_with_available_directions['square']
-        available_directions = square_with_available_directions['available_directions']
-        move = assign_move(square, available_directions)
+        squares_stack_item = squares_stack.pop()
+        square = squares_stack_item['square']
+        available_directions = squares_stack_item['available_directions']
+        move = get_best_individual_move(square, available_directions)
         target = game_map.get_target(square, move.direction)
         target_key = "{},{}".format(target.x, target.y)
         if target_key not in targets:
             targets[target_key] = [{'move': move, 'available_directions': available_directions}]
         else:
             existing_moves = targets[target_key]
-            moves = existing_moves + [{'move': move, 'available_directions': available_directions}]
-            if sum([m['move'].square.strength for m in moves]) <= 255 * 1.2:
+            cumulated_moves = existing_moves + [{'move': move, 'available_directions': available_directions}]
+            if sum([m['move'].square.strength for m in cumulated_moves]) <= MAX_STRENGTH * 1.2:
                 targets[target_key] = existing_moves + [{'move': move, 'available_directions': available_directions}]
             else:
                 if move.direction == STILL:
@@ -154,11 +124,11 @@ while True:
                             {'square': existing_move['move'].square, 'available_directions': directions})
                 else:
                     # sorting existing moves by decreasing strength
-                    moves.sort(
+                    cumulated_moves.sort(
                         key=lambda item: -1000 if item['move'].direction == STILL else -item['move'].square.strength)
                     total_strength = 0
                     top_moves = []
-                    for mv in moves:
+                    for mv in cumulated_moves:
                         strength = mv['move'].square.strength
                         if total_strength + strength <= 255:
                             total_strength += strength
@@ -170,4 +140,16 @@ while True:
                     targets[target_key] = top_moves
 
     moves = [target_move['move'] for target_moves in targets.values() for target_move in target_moves]
-    hlt.send_frame(moves)
+    return moves
+
+
+myID, game_map = hlt.get_init()
+productions = [sq.production for sq in game_map]
+MAX_PRODUCTION = max(productions)
+unowned_id = get_unowned_id()
+bot_name = os.path.basename(__file__).split('.')[0]
+hlt.send_init(bot_name)
+
+while True:
+    game_map.get_frame()
+    hlt.send_frame(get_best_collective_moves())
