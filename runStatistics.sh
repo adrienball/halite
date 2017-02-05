6e9f492112a34719935343cf27008716ca5bd083
Adrien Ball <adrien.ball@snips.net>
2017-02-05 23:41:46 +0100

    Handle equality in statistics script

--- runStatistics.sh
+++ runStatistics.sh
@@ -11,16 +11,12 @@ run_halite() {
     nb_bots=${#bot_names[@]}
     bot_1=${bot_names[0]}
     bot_2=${bot_names[1]}
-    if [ ${nb_bots} = 2 ];then
+    (( nb_bots == 2 )) &&
         ./halite -d "$1 $2" -q "python3 ${bot_1}.py" "python3 ${bot_2}.py"
-    elif [ ${nb_bots} = 3 ]; then
-        bot_3=${bot_names[2]}
+    (( nb_bots == 3 )) && bot_3=${bot_names[2]} &&
         ./halite -d "$1 $2" -q "python3 ${bot_1}.py" "python3 ${bot_2}.py" "python3 ${bot_3}.py"
-    elif [ ${nb_bots} = 4 ]; then
-        bot_3=${bot_names[2]}
-        bot_4=${bot_names[3]}
+    (( nb_bots == 4 )) && bot_3=${bot_names[2]} && bot_4=${bot_names[3]} &&
         ./halite -d "$1 $2" -q "python3 ${bot_1}.py" "python3 ${bot_2}.py" "python3 ${bot_3}.py" "python3 ${bot_4}.py"
-    fi
 }
 
 is_result_line() {
@@ -40,9 +36,7 @@ process_line() {
     bot_id=${elements[0]}
     bot_rank=${elements[1]}
     bot_last_alive=${elements[2]}
-    if [ ${bot_rank} = 1 ]; then
-        echo ${bot_id}
-    fi
+    echo "${bot_last_alive};"
 }
 
 width=$1
@@ -52,31 +46,37 @@ iterations=$3
 read -ra bot_names <<<$4
 nb_bots=${#bot_names[@]}
 
-if [[ ${nb_bots} < 2 || ${nb_bots} > 4 ]]; then
-    echo "Invalid number of bots: ${nb_bots}"
-fi
+(( nb_bots < 2 || nb_bots > 4 )) && echo "Invalid number of bots: ${nb_bots}"
 
-for iteration in `seq 1 ${iterations}`
-do
+for iteration in `seq 1 ${iterations}`; do
     printf "iteration ${iteration}: "
-    winner=$(run_halite ${width} ${height} "$4" |
+    last_alive=$(run_halite ${width} ${height} "$4" |
         while IFS= read -r line
         do
-            if is_result_line "$line" ; then
-                echo $(process_line "$line")
-            fi
+            is_result_line "$line" && echo $(process_line "$line")
         done |tr -d '\n')
-    printf  "winner is ${winner}\n"
+    last_alive="${last_alive%?}"
 
-    if [ ${winner} = 1 ]; then
-        bot_1_wins=$((${bot_1_wins} + 1))
-    elif [ ${winner} = 2 ]; then
-        bot_2_wins=$((${bot_2_wins} + 1))
-    elif [ ${winner} = 3 ]; then
-        bot_3_wins=$((${bot_3_wins} + 1))
-    elif [ ${winner} = 4 ]; then
-        bot_4_wins=$((${bot_4_wins} + 1))
-    fi
+    IFS=';' read -ra last_alive_array <<< "$last_alive"
+
+    max_alive=0
+    for alive in "${last_alive_array[@]}"; do
+        ((alive > max_alive )) && max_alive=${alive}
+    done
+
+    IFS=';' read -ra last_alive_array <<< "$last_alive"
+
+    index=1
+    for alive in "${last_alive_array[@]}"; do
+        if (( alive == max_alive )); then
+            (( index == 1 )) && bot_1_wins=$((${bot_1_wins} + 1)) && printf "1 "
+            (( index == 2 )) && bot_2_wins=$((${bot_2_wins} + 1)) && printf "2 "
+            (( index == 3 )) && bot_3_wins=$((${bot_3_wins} + 1)) && printf "3 "
+            (( index == 4 )) && bot_4_wins=$((${bot_4_wins} + 1)) && printf "4 "
+        fi
+        index=$((${index} + 1))
+    done
+    printf "\n"
 done
 
 printf "\n===========RESULTS=============\n"
@@ -84,12 +84,8 @@ ratio_1=$((bot_1_wins * 100 / iterations))
 printf "\t${bot_names[0]}: ${bot_1_wins} (${ratio_1}%%)\n"
 ratio_2=$((bot_2_wins * 100 / iterations))
 printf "\t${bot_names[1]}: ${bot_2_wins} (${ratio_2}%%)\n"
-if [[ ${nb_bots} > 2 ]]; then
-    ratio_3=$((bot_3_wins * 100 / iterations))
+(( nb_bots > 2 )) && ratio_3=$((bot_3_wins * 100 / iterations)) &&
     printf "\t${bot_names[2]}: ${bot_3_wins} (${ratio_3}%%)\n"
-fi
-if [[ ${nb_bots} > 3 ]]; then
-    ratio_4=$((bot_4_wins * 100 / iterations))
+(( nb_bots > 3 )) && ratio_4=$((bot_4_wins * 100 / iterations)) &&
     printf "\t${bot_names[3]}: ${bot_4_wins} (${ratio_4}%%)\n"
-fi
 printf "===============================\n"
